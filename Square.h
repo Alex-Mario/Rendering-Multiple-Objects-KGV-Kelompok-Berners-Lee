3472fe0a4ad791f48980389e2dca84c2abdd94b1
Alex Mario Simanjuntak <80665374+Archieved@users.noreply.github.com>
2022-05-30 19:27:29 +0700

    Update Square.h

--- Square.h
+++ Square.h
@@ -15,8 +15,11 @@ public:
 	glm::mat4x4 getTransformationMat4x4();
 	
 	void setTranslation(glm::vec3 newTranslation);
+	void setTranslation(float transX, float transY, float transZ);
+
 	void setRotation(float angle, glm::vec3 axis);
 	void setScale(glm::vec3 newScale);
+	void setScale(float scaleX, float scaleY, float scaleZ);
 
 	void draw();
 
