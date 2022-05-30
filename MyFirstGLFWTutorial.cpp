9f0b479ce05fa64ae24f9348f6a177e33463e3e1
Alex Mario Simanjuntak <80665374+Archieved@users.noreply.github.com>
2022-05-31 00:12:08 +0700

    Update MyFirstGLFWTutorial.cpp
    pembaharuan

--- MyFirstGLFWTutorial.cpp
+++ MyFirstGLFWTutorial.cpp
@@ -24,7 +24,7 @@ int main(int key, int scancode, int action, int mods) //main program
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1024, 768, "OOP Rendering Multiple Objects KGV Kelompok Berners Lee", NULL, NULL);
+    window = glfwCreateWindow(1280, 720, "OOP Rendering Multiple Objects KGV Kelompok Berners Lee", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -47,51 +47,66 @@ int main(int key, int scancode, int action, int mods) //main program
     s2.setColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
 
     Square s3; // kotak belakang
-    s3.setTranslation(glm::vec3(0.0f, 0.35f, 0.0f));
-    s3.setScale(glm::vec3(0.35f, 0.45f, 1.0f));
-    s3.setColor(glm::vec4(0.749f, 0.207f, 0.168f, 1.0f));
+    s3.setTranslation(glm::vec3(0.15f, 0.25f, 0.0f));
+    s3.setScale(glm::vec3(0.5f, 0.35f, 1.0f));
+    s3.setColor(glm::vec4(0.831f, 0.109f, 0.149f, 1.0f));
    
     Square s4; //ban1
-    s4.setTranslation(glm::vec3(-0.6f, -0.18f, 0.0f));
-    s4.setScale(glm::vec3(0.1f, 0.15f, 1.0f));
-    s4.setColor(glm::vec4(0.05f, 0.05f, 0.05f, 1.0f));
+    s4.setTranslation(glm::vec3(-0.6f, -0.15f, 0.0f));
+    s4.setScale(glm::vec3(0.1f, 0.115f, 1.0f));
+    s4.setColor(glm::vec4(0.368f, 0.368f, 0.368f, 1.0f));
+
+    Square s12; //isiban1
+    s12.setTranslation(glm::vec3(-0.6f, -0.15f, 0.0f));
+    s12.setScale(glm::vec3(0.03f, 0.03f, 1.0f));
+    s12.setColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
     
     Square s5; //ban2
-    s5.setTranslation(glm::vec3(-0.17f, -0.18f, 0.0f));
-    s5.setScale(glm::vec3(0.1f, 0.15f, 1.0f));
-    s5.setColor(glm::vec4(0.05f, 0.05f, 0.05f, 1.0f));
+    s5.setTranslation(glm::vec3(-0.1f, -0.15f, 0.0f));
+    s5.setScale(glm::vec3(0.1f, 0.115f, 1.0f));
+    s5.setColor(glm::vec4(0.368f, 0.368f, 0.368f, 1.0f));
+
+    Square s13; //isiban2
+    s13.setTranslation(glm::vec3(-0.1f, -0.15f, 0.0f));
+    s13.setScale(glm::vec3(0.03f, 0.03f, 1.0f));
+    s13.setColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
 
     Square s6; //ban3
-    s6.setTranslation(glm::vec3(0.19f, -0.18f, 0.0f));
-    s6.setScale(glm::vec3(0.1f, 0.15f, 1.0f));
-    s6.setColor(glm::vec4(0.05f, 0.05f, 0.05f, 1.0f));
+    s6.setTranslation(glm::vec3(0.4f, -0.15f, 0.0f));
+    s6.setScale(glm::vec3(0.1f, 0.115f, 1.0f));
+    s6.setColor(glm::vec4(0.368f, 0.368f, 0.368f, 1.0f));
+
+    Square s14; //isiban3
+    s14.setTranslation(glm::vec3(0.4f, -0.15f, 0.0f));
+    s14.setScale(glm::vec3(0.03f, 0.03f, 1.0f));
+    s14.setColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
     
     Square s7; //knalpot
-    s7.setTranslation(glm::vec3(0.39f, 0.05f, 0.0f));
+    s7.setTranslation(glm::vec3(0.69f, 0.07f, 0.0f));
     s7.setScale(glm::vec3(0.04f, 0.03f, 1.0f));
-    s7.setColor(glm::vec4(0.501f, 0.501f, 0.501f, 1.0f));
+    s7.setColor(glm::vec4(0.925f, 0.607f, 0.494f, 1.0f));
     
     Square s8; //asap1
-    s8.setRotation(45.0f, glm::vec3(0.01f, 0.01f, 0.01f));
-    s8.setTranslation(glm::vec3(0.50f, -0.4f, 0.0f));
-    s8.setScale(glm::vec3(0.018f, 0.032f, 1.0f));
+    s8.setRotation(45.0f, glm::vec3(1.0f, 0.00f, 1.0f));
+    s8.setTranslation(glm::vec3(0.6f, -0.55f, 0.0f));
+    s8.setScale(glm::vec3(0.018f, 0.018f, 1.0f));
     s8.setColor(glm::vec4(0.882f, 0.878f, 0.874f, 1.0f));
 
     Square s9; //asap2
-    s9.setRotation(45.0f, glm::vec3(0.01f, 0.01f, 0.01f));
-    s9.setTranslation(glm::vec3(0.60f, -0.5f, 0.0f));
-    s9.setScale(glm::vec3(0.023f, 0.046f, 1.0f));
+    s9.setRotation(45.0f, glm::vec3(1.0f, 0.00f, 1.0f));
+    s9.setTranslation(glm::vec3(0.66f, -0.6f, 0.0f));
+    s9.setScale(glm::vec3(0.023f, 0.023f, 1.0f));
     s9.setColor(glm::vec4(0.882f, 0.878f, 0.874f, 1.0f));
 
     Square s10; //asap3
-    s10.setRotation(45.0f, glm::vec3(0.01f, 0.01f, 0.01f));
-    s10.setTranslation(glm::vec3(0.70f, -0.6f, 0.0f));
-    s10.setScale(glm::vec3(0.03f, 0.06f, 1.0f));
+    s10.setRotation(45.0f, glm::vec3(1.0f, 0.00f, 1.0f));
+    s10.setTranslation(glm::vec3(0.72f, -0.65f, 0.0f));
+    s10.setScale(glm::vec3(0.03f, 0.03f, 1.0f));
     s10.setColor(glm::vec4(0.882f, 0.878f, 0.874f, 1.0f));
 
     Square s11; //aspal
     s11.setScale(glm::vec3(1.0f, 0.5f, 1.0f));
-    s11.setColor(glm::vec4(0.368f, 0.368f, 0.368f, 1.0f));
+    s11.setColor(glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
 
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -122,7 +137,7 @@ int main(int key, int scancode, int action, int mods) //main program
 
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.235f, 0.890f, 0.984f, 1.0f); // warna window biru langit
+        glClearColor(0.745f, 0.803f, 0.901f, 1.0f); // warna window biru langit
 
         glUseProgram(program);
 
@@ -171,6 +186,18 @@ int main(int key, int scancode, int action, int mods) //main program
         glUniform4fv(uColor, 1, &s10.getColor()[0]);
         glUniformMatrix4fv(uMat4x4, 1, GL_FALSE, &s10.getTransformationMat4x4()[0][0]);
         s10.draw();
+
+        glUniform4fv(uColor, 1, &s12.getColor()[0]);
+        glUniformMatrix4fv(uMat4x4, 1, GL_FALSE, &s12.getTransformationMat4x4()[0][0]);
+        s12.draw();
+
+        glUniform4fv(uColor, 1, &s13.getColor()[0]);
+        glUniformMatrix4fv(uMat4x4, 1, GL_FALSE, &s13.getTransformationMat4x4()[0][0]);
+        s13.draw();
+
+        glUniform4fv(uColor, 1, &s14.getColor()[0]);
+        glUniformMatrix4fv(uMat4x4, 1, GL_FALSE, &s14.getTransformationMat4x4()[0][0]);
+        s14.draw();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
